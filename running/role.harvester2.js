e89951c44bf274527eeb838733dcbfa38b88db7d
ConradBBD <conrad@bbd.co.za>
2022-10-12 17:28:40 +0200

    harvester2 drop when source empty

--- running/role.harvester2.js
+++ running/role.harvester2.js
@@ -7,22 +7,12 @@ var roleHarvester2 = {
             if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[1], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
+            if (sources[1].energy == 0) {
+                creep.drop(RESOURCE_ENERGY);
+            }
         }
         else {
             creep.drop(RESOURCE_ENERGY);
-            // var targets = creep.room.find(FIND_STRUCTURES, {
-            //         filter: (structure) => {
-            //             return (structure.structureType == STRUCTURE_EXTENSION ||
-            //                     structure.structureType == STRUCTURE_SPAWN ||
-            //                     structure.structureType == STRUCTURE_TOWER) && 
-            //                     structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-            //         }
-            // });
-            // if(targets.length > 0) {
-            //     if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-            //         creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-            //     }
-            // }
         }
 	}
 };
