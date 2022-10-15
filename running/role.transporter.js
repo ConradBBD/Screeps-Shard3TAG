6774809c093513d3a09cd62172a5ed4fe4f0aad1
ConradBBD <conrad@bbd.co.za>
2022-10-15 10:10:23 +0200

    transporters

---
+++ running/role.transporter.js
@@ -0,0 +1,65 @@
+var roleTransorter = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+	    if(creep.store.getUsedCapacity() == 0) {
+            const target = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES);
+            if(target) {
+                if(creep.pickup(target) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target);
+                }
+            }
+        }
+        else {
+            var spawnTargets = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return structure.structureType == STRUCTURE_SPAWN && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                    }
+                });
+            var extensionTargets = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return structure.structureType == STRUCTURE_EXTENSION && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                }
+            });
+            var storageTargets = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return structure.structureType == STRUCTURE_STORAGE && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                }
+            });
+            var otherTargets = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_CONTAINER ||
+                                structure.structureType == STRUCTURE_TOWER) && 
+                                structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                    }
+            });
+            if (spawnTargets.length > 0) {
+                if(creep.transfer(spawnTargets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(spawnTargets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            } else if (extensionTargets.length > 0) {
+                if(creep.transfer(extensionTargets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(extensionTargets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            } else if (storageTargets.length > 0) {
+                if(creep.transfer(storageTargets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(storageTargets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            } else if (otherTargets.length > 0) {
+                if(creep.transfer(otherTargets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(otherTargets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            }
+        }
+	}
+};
+
+function removeItemOnce(arr, value) {
+    var index = arr.indexOf(value);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+    return arr;
+  }
+
+module.exports = roleTransorter;
